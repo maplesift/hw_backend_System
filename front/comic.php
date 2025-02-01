9301159dbb4d07ee8dc8cb61568a6267ad204b62
maplesift <a23050716@gmail.com>
2025-02-01 09:42:53 +0800

    hw front 04

--- front/comic.php
+++ front/comic.php
@@ -1,10 +1,69 @@
-<div class="col-6 ">
+<div class="col-7 inner">
     <h2 class="title">官方漫畫</h2>
-    <div class="p-4 bg-yellow">
-        <ul>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-        </ul>
+    <hr>
+    <div class="p-4 ">
+        <?php
+
+                // 分頁
+                $div=2;
+                $total=$Comic->count();
+                $pages=ceil($total/$div);
+                $now=$_GET['p']??1;
+                $start=($now-1)*$div;
+
+                // 空格很重要
+                $rows=$Comic->all(['sh'=>1]," limit $start,$div");
+                
+
+        // $rows=$Comic->all(['sh'=>1], "limit 2");
+        // *********
+        foreach($rows as $row):
+        ?>
+        <div>
+            <img src="./upload/<?=$row['img'];?>" class="img-fluid mt-2" style="width:498px;height:889px;">
+
+        </div>
+        <?php endforeach ;?>
+        <div class="cent">
+            <?php
+                if(($now-1)>0) {
+                    $prev=$now-1;
+                    echo "<a href='?do=$do&p=$prev'> <i class='bi bi-arrow-left-square'></i> </a>";
+                    
+                }
+
+                for($i=1;$i<=$pages;$i++){
+                    $size=($i==$now)?"24px":"16px";
+                    echo "<a href='?do=$do&p=$i' style='font-size:$size'>";
+                    // echo  $i;
+                    echo  "<span class=' bi bi-$i-square'> </span>";
+                    echo " </a>";
+                }
+                if(($now+1)<=$pages) {
+                    $next=$now+1;
+                    echo "<a href='?do=$do&p=$next'> <i class='bi bi-arrow-right-square'> </i> </a>";
+                    
+                }
+                ?>
+        </div>
+
+        <!--event-banner-box  -->
+        <div class="event-banner-box">
+            <div class="inner">
+            </div>
+            <ul>
+                <li>
+                    <a href="https://www.youtube.com/@bluearchive_tw" target="_blank" rel="noopener noreferrer">
+                        <img src="./icon/04820x197.jpg" alt="" class="img-fluid ">
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.facebook.com/TW.BlueArchive/" target="_blank" rel="noopener noreferrer">
+                        <img src="./icon/05820x197.jpg" alt="" class="img-fluid ">
+                    </a>
+                </li>
+            </ul>
+        </div>
+
     </div>
 </div>
